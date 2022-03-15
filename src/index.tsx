96b92a4573aa70b43d80cc2623e8da9777947b45
Blazej Celmer <blazej.celmer@guestline.com>
2022-03-15 15:54:38 +0100

    Moves store provider from index to App

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
