8ed1eda5b339fe7431637163977e89a597780cbd
Blazej Celmer <blazej.celmer@guestline.com>
2022-03-16 16:50:09 +0100

    Add test for adults capacity filter

--- src/app-tests/App.test.js
+++ src/app-tests/App.test.js
@@ -1,9 +1,14 @@
-import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "../App";
 import { hotelDetails, hotelList } from "./apiData";
 
-const waitForLoading = () => waitForElementToBeRemoved(() => screen.getByTestId("loading"))
+const waitForLoading = () =>
+  waitForElementToBeRemoved(() => screen.getByTestId("loading"));
 
 it("one big test", async () => {
   fetch.mockResponses(
@@ -14,25 +19,35 @@ it("one big test", async () => {
   render(<App />);
 
   // displays hotels with rooms
-  await waitForLoading()
+  await waitForLoading();
   expect(screen.getByText(/obm hotel 1/i)).toBeInTheDocument();
   expect(screen.getByText(/deluxe twin/i)).toBeInTheDocument();
   expect(screen.getByText(/obm hotel 2/i)).toBeInTheDocument();
 
-  
   // filters by rating
-  userEvent.click(screen.getByTestId("star-button-5"))
-  await waitForLoading()
+  userEvent.click(screen.getByTestId("star-button-5"));
+  await waitForLoading();
 
   expect(screen.queryByText(/obm hotel 1/i)).not.toBeInTheDocument();
   expect(screen.getByText(/obm hotel 2/i)).toBeInTheDocument();
 
-  userEvent.click(screen.getByTestId("star-button-1"))
-  await waitForLoading()
+  userEvent.click(screen.getByTestId("star-button-1"));
+  await waitForLoading();
   expect(screen.getByText(/obm hotel 1/i)).toBeInTheDocument();
   expect(screen.getByText(/obm hotel 2/i)).toBeInTheDocument();
 
-  
+  // filters by adults capacity
+  expect(screen.getAllByText(/adults: 1/i).length).toBeGreaterThan(0);
+  expect(screen.getAllByText(/adults: 2/i).length).toBeGreaterThan(0);
+  userEvent.click(screen.getByRole("button", { name: "Adults:-plus" }));
+  await waitForLoading();
+
+  expect(screen.queryByText(/adults: 1/i)).not.toBeInTheDocument();
+  expect(screen.getAllByText(/adults: 2/i).length).toBeGreaterThan(0);
+
+  userEvent.click(screen.getByRole("button", { name: "Adults:-minus" }));
+  await waitForLoading();
+
+  expect(screen.getAllByText(/adults: 1/i).length).toBeGreaterThan(0);
+  expect(screen.getAllByText(/adults: 2/i).length).toBeGreaterThan(0);
 });
-
-
