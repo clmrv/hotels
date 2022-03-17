23161c5a5a172d69da3149cd25ba851ec55dc2af
Blazej Celmer <blazej.celmer@guestline.com>
2022-03-17 16:46:51 +0100

    Add rating part to the e2e test

--- src/e2e-tests/index.js
+++ src/e2e-tests/index.js
@@ -4,4 +4,18 @@ describe("Filtering e2e test", () => {
     browser.expect.elements("section").count.to.equal(4);
     browser.expect.element("main").text.to.match(/deluxe twin/i);
   });
+
+  it("filters hotels by rating", () => {
+    browser.click("[data-testid=star-button-5]");
+    browser.waitForElementNotPresent("[data-testid=loading]");
+
+    browser.expect.element("main").text.not.to.match(/obm hotel 1/i);
+    browser.expect.element("main").text.to.match(/obm hotel 2/i);
+
+    browser.click("[data-testid=star-button-1]");
+    browser.waitForElementNotPresent("[data-testid=loading]");
+
+    browser.expect.element("main").text.to.match(/obm hotel 1/i);
+    browser.expect.element("main").text.to.match(/obm hotel 2/i);
+  });
 });
